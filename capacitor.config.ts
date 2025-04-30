5f4f8af69ce5cb88719cf3cac501284a36b3a14f
Leonardo <leonardo.hernandez@tigears.com>
2025-04-29 21:18:13 -0400

    update ready for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'weather-app',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      style: "DARK",
+      backgroundColor: "#ffffffff",
+    },
+  },
 };
 
 export default config;
